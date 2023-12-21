ea1da023fe01fa656e4039bc52f57568851f7143
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2023-12-21 12:45:29 +0000

    close to having it calcing...

--- R/calc_morphometry.R
+++ R/calc_morphometry.R
@@ -3,28 +3,34 @@ source(here::here("R/packages.R"))
 # Read in data
 flooded_lands <- arrow::open_dataset(here("data/flooded"), partitioning = "stusps") |> 
   sfarrow::read_sf_dataset() |>
-  st_transform(5072)
+  st_transform(5072) 
 
-# Read in data - below is just an example to get started.
-lakes <- st_read(here("data/flooded_lands_inventory.gpkg")) |>
-  st_transform(5072)
-lakes_sub <- st_make_valid(lakes[1000:1010,])
+small <- split(flooded_lands[1:5,], 1:nrow(flooded_lands[1:5,]))
+
+flooded_lands_l <- split(flooded_lands, 1:nrow(flooded_lands))
 
 morph_it <- function(lake) {
- 
-  elev <- elevatr::get_elev_raster(lake, 12)
+  lake <- st_make_valid(lake)
+  elev <- suppressMessages(elevatr::get_elev_raster(lake, 12, override_size_check = TRUE))
   lake_lm <- lakemorpho::lakeSurroundTopo(lake, elev)
   perim <- lakeShorelineLength(lake_lm)
   num_pts <- round(perim/50)
   if(num_pts > 1000) {num_pts <- 1000}
   metrics <- calcLakeMetrics(lake_lm, 0, num_pts)
   comid <- select(lake, comid, globalid, objectid)
-  bind_cols(comid, round(data.frame(metrics), 2))
+  sf <- bind_cols(comid, round(data.frame(metrics), 2))
+  df <- sf
+  st_geometry(df) <- NULL
+  id <- c(df$comid, df$objectid, df$globalid)
+  id <- id[!is.na(id)]
+  data.table::fwrite(df,paste0(here("data/metrics/metrics"), id, ".csv"))
+  sf
 }
 
-plan(multisession, workers = 6)
-morpho_metrics <- future_lapply(split(lakes_sub, 1:nrow(lakes_sub)), morph_it, 
+plan(multisession)
+morpho_metrics <- future_lapply(flooded_lands_l, morph_it, 
                                 future.seed=TRUE)
 
-bind_rows(morpho_metrics)
+flooded_lands_morpho <-bind_rows(morpho_metrics)
+st_write(flooded_lands_morpho, "flooded_lands_morpho.gpkg")
        
